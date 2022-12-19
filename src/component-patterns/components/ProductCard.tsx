4e58d0d55611c252363e61a9842c73ef1b81b16e
margaritasing <libiamfreitesm@gmail.com>
2022-12-19 15:24:03 -0300

    empezando a modificar el componente

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -1,13 +1,27 @@
 import style from '../styles/styles.module.css'
 import noImage from '../assets/no-image.jpg'
+import { useState } from 'react'
 
 
 const ProductCard = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className={style.productCard}>
         <img className={style.productImg} src="./coffee-mug.png" alt="Coffee Mug" />
         {/* <img className={style.productImg} src={noImage} alt="Coffee Mug" /> */}
         <span className={style.productDescription}>Coffee Mug</span>
+
+        <div className={ style.buttonsContainer}>
+
+            <button className={style.buttonMinus}>-</button>
+        
+            <div className={style.countLabel}> { counter }</div>
+
+            <button className={style.buttonAdd}> + </button>
+
+        </div>
     </div>
   )
 }
