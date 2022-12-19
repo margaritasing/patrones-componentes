a67a35bf24f830b6f7203bdc58a3c655070d1063
margaritasing <libiamfreitesm@gmail.com>
2022-12-19 16:20:26 -0300

    paso de props de hijo a padre

--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,19 @@
-import ProductCard from "../components/ProductCard"
+import ProductCard from "../components/ProductCard";
+
+const product = {
+    id:'1',
+    title:'Coffee Mug - Card'
+
+}
 
 export const Shoppingpage = () =>{
     return (
         <div>
             <h1>Shopping Store</h1>
             <hr />
-            <ProductCard/>
+            <div style={{display:'flex', flexDirection:'row',flexWrap:'wrap' }}>
+                <ProductCard product={ product } />                
+            </div>
         </div>
 
     )
