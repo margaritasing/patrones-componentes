3f627572149ccb8bb002822eae2741a02eaa4369
margaritasing <libiamfreitesm@gmail.com>
2022-12-20 18:47:15 -0300

    creo el componente para el title

---
+++ src/component-patterns/components/ProductTitle.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react"
+import { ProductContext } from "./ProductCard"
+import style from '../styles/styles.module.css';
+
+
+export const ProductTitle = ({title}: {title?:string}) => {
+
+    const {product} = useContext(ProductContext)
+  
+    return (
+      <span className={style.productDescription}>
+        {title ? title : product.title}
+      </span>
+    )  
+  }
