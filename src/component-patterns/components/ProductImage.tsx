bece77cf37346b065ad04d458a4d74b847371f7b
margaritasing <libiamfreitesm@gmail.com>
2022-12-20 18:47:00 -0300

    creo el componente para la imagen

---
+++ src/component-patterns/components/ProductImage.tsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { ProductContext } from "./ProductCard";
+import style from '../styles/styles.module.css';
+import noImage from '../assets/no-image.jpg';
+
+export const ProductImage = ({img = '' }) => {
+
+    const { product } = useContext( ProductContext )
+  
+    let imgToShow: string;
+  
+    if (img) {
+      imgToShow = img;   
+    }else if (product.img) {
+      imgToShow = product.img;  
+    }else{
+      imgToShow = noImage;
+    }
+  
+    return (
+      <img className={style.productImg} src={ imgToShow } alt="Product-img" />  
+    )
+  }
