7d07305ad9a25e10c1230c28f62716026b39bb34
margaritasing <libiamfreitesm@gmail.com>
2022-12-20 18:58:25 -0300

    commit # 1000, agrego el index, agrupo componentes

---
+++ src/component-patterns/components/index.ts
@@ -0,0 +1,4 @@
+export { ProductCard } from './ProductCard';
+export { ProductButton } from './ProductButton';
+export { ProductImage } from './ProductImage';
+export { ProductTitle } from './ProductTitle';
